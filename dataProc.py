fd6c70971e4d0ff805493bfaceef76e5b95a6dcc
JIAOJM\jiao <jiaojm@outlook.com>
2018-03-28 19:27:32 +0900

    check result value

---
+++ dataProc.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import cv2
+import glob
+import os
+import keras
+from keras.preprocessing.image import array_to_img, img_to_array
+
+def npyToJpg(src = "20171225-10", dst = "20180228"):
+    max_a = 19.766789
+    min_a = -11.055192
+    max_b = 17.245272
+    min_b = -88.138232
+    for i in range(0, 1000, 1):
+        npy = numpy.load("/mnt/recordings/SimulationResults/2D/"+src+"/phie_"+"%04d"%i+".npy")
+        npy = 255*(npy-min_a)/(max_a-min_a)
+        cv2.imwrite("/mnt/recordings/SimulationResults/mapping/"+dst+"/data/A/val/%04d"%i+".jpg",npy)
+        npy = numpy.load("/mnt/recordings/SimulationResults/2D/"+src+"/vmem_"+"%04d"%i+".npy")
+        npy = 255*(npy-min_b)/(max_b-min_b)
+        cv2.imwrite("/mnt/recordings/SimulationResults/mapping/"+dst+"/data/B/val/%04d"%i+".jpg",npy)
+
+def loadData(inputPath, cvtDataType = 0, resize = 0, rawRows = 200, rawCols = 200, imgRows = 256, imgCols = 256, channels = 1):
+    fileName = glob.glob(inputPath)
+    if resize == 0:
+        mergeImg = np.ndarray((len(fileName), rawRows, rawCols, channels), dtype=np.uint8)
+    else:
+        mergeImg = np.ndarray((len(fileName), imgRows, imgCols, channels), dtype=np.uint8)
+    rawImg = np.ndarray((rawRows, rawCols, channels), dtype=np.uint8)
+    tempImg = np.ndarray((rawRows, rawCols, channels), dtype=np.uint8)
+    j = 0
+    for i in fileName:
+        rawImg = cv2.imread(i, 0)
+        if resize == 1:
+            tempImg = vc2.resize(rawImg, imgRows, imgCols)
+        mergeImg[j] = img_to_array(tempImg)
+        j += 1
+    if cvtDataType == 1:
+        mergeImg = mergeImg.astype('float64')
+        mergeImg /= 255
+    return mergeImg
--- monopolar.py
+++ monopolar.py
@@ -1,6 +1,37 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import cv2
+import cv2 as cv
+import dataProc
+import numpy as np
+import glob
+import math
 
-def averagely(electrodeNum = 16):
+electrodesNum = 25
+imgRows = 200
+imgCols = 200
+channels = 1
+srcPath = '/mnt/recordings/SimulationResults/mapping/2D/train/extra/*.jpg'
+dstPath = '/mnt/recordings/SimulationResults/mapping/2D/train/extra_sparse'
+dstStartNum = 100
+fileName = glob.glob(srcPath)
+src = np.ndarray((len(fileName), imgRows, imgCols, channels), dtype = np.float64)
+src = dataProc.loadData(inputPath = srcPath)
+kernelSize = 3
+gaussianKernel = np.array([[1/16, 2/16, 1/16], [2/16, 4/16, 2/16], [1/16, 2/16, 1/16]])
+temp = np.zeros((kernelSize, kernelSize, channels), np.float64)
+stride = int(imgRows/(round(math.sqrt(electrodesNum))-1)-1+0.5)
+dst = np.ndarray((len(fileName), imgRows, imgCols, channels), dtype = np.float64)
+for imgNum in range(len(fileName)):
+    for row in range(0, int(math.sqrt(electrodesNum)+0.5), stride):
+        for col in range(0, int(math.sqrt(electrodesNum)+0.5), stride):
+            temp = src[imgNum, row:row+kernelSize, col:col+kernelSize, :]
+            cv.filter2D(temp, cv.CV_64F, gaussianKernel)
+            temp[:, :, 0] = temp[int(kernelSize/2)+1, int(kernelSize/2)+1, 0]
+            dst[imgNum, row:row+kernelSize, col:col+kernelSize, :] = temp
+dst = dst.astype('float64')
+for i in range(0, len(fileName)):
+    dstFileName = dstPath + '/%d'%electrodesNum + '/%04d'%dstStartNum + '.jpg'
+    cv.imwrite(dstFileName, dst[i])
+    dstStartNum += 1
+print('finished')
