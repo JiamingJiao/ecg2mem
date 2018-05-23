06f2602ad3a7c6103ee26c2a37e3c2c212877d1d
JiamingJiao <jiaojm@outlook.com>
2018-05-23 20:36:20 +0900

    fixed netD
    Fixed bug about netD and use vgg16 as netD.
    Add sparse pseudoECG generator (not tested).

--- dataProc.py
+++ dataProc.py
@@ -19,12 +19,12 @@ def npyToPng(srcPath, dstPath):
     npy = loadData(srcPath)
     min = np.amin(npy)
     max = np.amax(npy)
-    for i in range(0, 1000, 1):
-        npy[i, :, :] = 255*(npy[i, :, :]-min)/(max-min)
+    npy = 255*(npy-min)/(max-min)
+    for i in range(0, npy.shape[0]):
         cv.imwrite(dstPath + "%04d"%i+".png",npy[i, :, :])
     print('completed')
 
-def loadData(inputPath, startNum = 0, resize = 0, rawRows = 200, rawCols = 200, imgRows = 256, imgCols = 256, channels = 1):
+def loadData(inputPath, startNum = 0, resize = 0, rawRows = 200, rawCols = 200, imgRows = 256, imgCols = 256, normalization = 0):
     fileName = glob.glob(inputPath + '*.npy')
     if resize == 0:
         mergeImg = np.ndarray((len(fileName), rawRows, rawCols), dtype = np.float64)
@@ -43,11 +43,15 @@ def loadData(inputPath, startNum = 0, resize = 0, rawRows = 200, rawCols = 200, 
             #mergeImg[i] = img_to_array(rawImg)
             mergeImg[i] = rawImg
         startNum += 1
+    if normalization == 1:
+        min = np.amin(mergeImg)
+        max = no.amax(mergeImg)
+        mergeImg = (mergeImg-min)/(max-min)
     return mergeImg
-
+'''
 def generatePseudoECG(srcPath, dstPath, electrodesNum, zDistance = 100, interpolationSize = (200, 200)):
     src = loadData(inputPath = srcPath, startNum = 0, resize = 0)
-    dst = np.ndarray(src.shape, dtype = np.float64)
+    dst = np.ndarray((src.shape[1], src.shape[2]), dtype = np.float64)
     diffVKernel = np.zeros((3, 3, 1), dtype = np.float64)
     diffVKernel[1, :, 0] = 1
     diffVKernel[:, 1, 0] = 1
@@ -65,8 +69,8 @@ def generatePseudoECG(srcPath, dstPath, electrodesNum, zDistance = 100, interpol
     for i in range(0, src.shape[0]):
         diffV = cv.filter2D(src = src[i], ddepth = -1, kernel = diffVKernel, dst = diffV, anchor = (-1, -1), delta = 0, borderType = cv.BORDER_REPLICATE)
         for siteNum in range(0, electrodesNum):
-            cellRow = math.floor(siteNum/math.sqrt(electrodesNum)+0.1)
-            cellCol = siteNum%math.floor(math.sqrt(electrodesNum)+0.1)
+            cellRow = math.floor(siteNum/math.sqrt(electrodesNum)+1e-5)
+            cellCol = siteNum%math.floor(math.sqrt(electrodesNum)+1e-5)
             rowDistance = rowIndex - cellRow * stride
             colDistance = colIndex - cellCol * stride
             distance = cv.sqrt(cv.magnitude(rowDistance, rowDistance) + cv.magnitude(colDistance, colDistance) + zDistanceSquare)
@@ -75,3 +79,72 @@ def generatePseudoECG(srcPath, dstPath, electrodesNum, zDistance = 100, interpol
         dstFileName = dstPath + '%04d'%i
         np.save(dstFileName, interpolatedECG)
     print('completed')
+'''
+
+# generate full size pseudo-ECG maps, and downsample them if it is necessary
+def generatePseudoECG(srcPath, dstPath):
+    src = loadData(inputPath = srcPath, startNum = 0, resize = 0)
+    dst = np.ndarray(src.shape, dtype = np.float64)
+    diffVKernel = np.zeros((3, 3, 1), dtype = np.float64)
+    diffVKernel[1, :, 0] = 1
+    diffVKernel[:, 1, 0] = 1
+    diffVKernel[1, 1, 0] = -4
+    diffV = np.ndarray((src.shape[1], src.shape[2]), dtype = np.float64)
+    distance = np.ndarray((src.shape[1], src.shape[2]), dtype = np.float64)
+    pseudoECG = np.ndarray((src.shape[1], src.shape[2]), dtype = np.float64)
+    firstRowIndex = np.linspace(0, src.shape[1], num = src.shape[1], endpoint = False)
+    firstColIndex = np.linspace(0, src.shape[2], num = src.shape[2], endpoint = False)
+    colIndex, rowIndex = np.meshgrid(firstRowIndex, firstColIndex)
+    for i in range(0, src.shape[0]):
+        diffV = cv.filter2D(src = src[i], ddepth = -1, kernel = diffVKernel, dst = diffV, anchor = (-1, -1), delta = 0, borderType = cv.BORDER_REPLICATE)
+        for row in range(0, src.shape[1]):
+            for col in range(0, src.shape[2]):
+                distance = cv.magnitude((rowIndex-row), (colIndex-col))
+                pseudoECG[row,col] = cv.sumElems(cv.divide(diffV, distance))[0]
+        dstFileName = dstPath + '%04d'%i
+        np.save(dstFileName, pseudoECG)
+    print('completed')
+
+def downSample(srcPath, dstPath, samplePoints = (5, 5), interpolationSize = (200, 200)):
+    src = loadData(srcPath)
+    rowStride = math.floor(src.shape[1]/samplePoints[0])
+    colStride = math.floor(src.shape[2]/samplePoints[1])
+    multipleOfStride = ((samplePoints[0]-1)*rowStride+1, (samplePoints[1]-1)*colStride+1)
+    temp = np.ndarray(multipleOfStride, dtype = np.float64) #Its size is a multiple of stride + 1
+    sample = np.ndarray(samplePoints, dtype = np.float64)
+    for i in range(0, src.shape[0]):
+        temp = cv.resize(src[i, :, :], multipleOfStride)
+        for j in range(0, samplePoints[0]):
+            for k in range(0, samplePoints[1]):
+                sample[j, k] = temp[j*rowStride, k*colStride]
+        interpolated = cv.resize(sample, interpolationSize)
+        dstFileName = dstPath + '%04d'%i
+        np.save(dstFileName, interpolated)
+    print('down sampling completed')
+
+def generateSparsePseudoECG(srcPath, dstPath, samplePoints = (10, 10)):
+    src = loadData(srcPath)
+    rowStride = math.floor(src.shape[1]/samplePoints[0])
+    colStride = math.floor(src.shape[2]/samplePoints[1])
+    multipleOfStride = ((samplePoints[0]-1)*rowStride+1, (samplePoints[1]-1)*colStride+1)
+    temp = np.ndarray(multipleOfStride, dtype = np.float64) #Its size is a multiple of stride + 1
+    sample = np.ndarray(samplePoints, dtype = np.float64)
+    diffVKernel = np.zeros((3, 3, 1), dtype = np.float64)
+    diffVKernel[1, :, 0] = 1
+    diffVKernel[:, 1, 0] = 1
+    diffVKernel[1, 1, 0] = -4
+    diffV = np.ndarray((src.shape[1], src.shape[2]), dtype = np.float64)
+    firstRowIndex = np.linspace(0, src.shape[1], num = src.shape[1], endpoint = False)
+    firstColIndex = np.linspace(0, src.shape[2], num = src.shape[2], endpoint = False)
+    colIndex, rowIndex = np.meshgrid(firstRowIndex, firstColIndex)
+    pseudoECG = np.ndarray(samplePoints, dtype = np.float64)
+    interpolated = np.ndarray(src.shape, dtype = np.float64)
+    for i in range(0, src.shape[0]):
+        diffV = cv.filter2D(src = src[i], ddepth = -1, kernel = diffVKernel, dst = diffV, anchor = (-1, -1), delta = 0, borderType = cv.BORDER_REPLICATE)
+        temp = cv.resize(src[i, :, :], multipleOfStride)
+        for row in range(0, temp.shape[0]):
+            for col in range(0, temp.shape[1]):
+                distance = cv.magnitude((rowIndex-row*stride), (colIndex-col*stride))
+                pseudoECG[row, col] = cv.sumElems(cv.divide(diffV, distance))[0]
+        interpolated[i, :, :] = cv.resize(pseudoECG, (src.shape[1], src.shape[2]))
+    return interpolated
--- model.py
+++ model.py
@@ -2,13 +2,13 @@
 # -*- coding: utf-8 -*-
 
 import numpy as np
-import cv2
+import cv2 as cv
 import os
 import glob
 import keras
 from keras.preprocessing.image import array_to_img, img_to_array, load_img
 from keras.models import *
-from keras.layers import Input, merge, Conv2D, UpSampling2D, Dropout, BatchNormalization, Flatten, Dense
+from keras.layers import Input, merge, Conv2D, UpSampling2D, Dropout, BatchNormalization, Flatten, Dense, MaxPooling2D
 from keras.optimizers import *
 from keras.callbacks import ModelCheckpoint, LearningRateScheduler
 from keras import backend
@@ -85,7 +85,7 @@ class networks(object):
     def netD(self):
         inputA = Input((self.imgRows, self.imgCols, self.channels))
         inputB = Input((self.imgRows, self.imgCols, self.channels))
-        combinedImg = merge([inputA, inputB], mode = 'concat', concat_axis = 2)
+        combinedImg = merge([inputA, inputB], mode = 'concat', concat_axis = -1)
         conv1 = Conv2D(self.dKernels, 1, padding = 'same', kernel_initializer = 'he_normal')(combinedImg)
         conv1 = LeakyReLU(alpha = 0.2)(conv1)
         conv2 = Conv2D(self.dKernels*2, 1, padding = 'same', kernel_initializer = 'he_normal')(conv1)
@@ -94,20 +94,52 @@ class networks(object):
         conv3 = Conv2D(1, 1, padding = 'same', kernel_initializer = 'he_normal')(conv2)
         conv4 = Conv2D(1, 1, activation = 'sigmoid')(conv3)
         flat = Flatten()(conv4)
-        probability = Dense(1, activation = 'softmax')(flat)
+        probability = Dense(2, activation = 'softmax')(flat)
         model = Model(input = [inputA, inputB], output = probability, name='netD')
         #model.compile(optimizer = Adam(lr = 1e-4), loss = 'binary_crossentropy', metrics = ['accuracy'])
+        return model
+
+    def vgg16(self):
+        inputA = Input((self.imgRows, self.imgCols, self.channels))
+        inputB = Input((self.imgRows, self.imgCols, self.channels))
+        combinedImg = merge([inputA, inputB], mode = 'concat', concat_axis = -1)
+        conv1 = Conv2D(self.dKernels, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(combinedImg)
+        conv1 = Conv2D(self.dKernels, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv1)
+        pool1 = MaxPooling2D((2, 2))(conv1)
+        conv2 = Conv2D(self.dKernels*2, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool1)
+        conv2 = Conv2D(self.dKernels*2, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv2)
+        pool2 = MaxPooling2D((2, 2))(conv2)
+        conv3 = Conv2D(self.dKernels*4, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool2)
+        conv3 = Conv2D(self.dKernels*4, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv3)
+        conv3 = Conv2D(self.dKernels*4, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv3)
+        pool3 = MaxPooling2D((2,2))(conv3)
+        conv4 = Conv2D(self.dKernels*8, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool3)
+        conv4 = Conv2D(self.dKernels*8, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv4)
+        conv4 = Conv2D(self.dKernels*8, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv4)
+        pool4 = MaxPooling2D((2,2))(conv4)
+        conv5 = Conv2D(self.dKernels*8, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool4)
+        conv5 = Conv2D(self.dKernels*8, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv5)
+        conv5 = Conv2D(self.dKernels*8, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv5)
+        pool5 = MaxPooling2D((2,2))(conv5)
+        flatten6 = Flatten()(pool5)
+        dense6 = Dense(self.dKernels*64, activation = 'relu')(flatten6)
+        dense6 = Dense(self.dKernels*64, activation = 'relu')(dense6)
+        probability = Dense(2, activation = 'softmax')(dense6)
+        model = Model(input = [inputA, inputB], output = probability, name='VGG16')
+        #model = Model(input = inputB, output = probability, name='VGG16')
         return model
 
     def netA(self):
         inputA = Input((self.imgRows, self.imgCols, self.channels))
         inputB = Input((self.imgRows, self.imgCols, self.channels))
         netG = self.uNet()
-        netD = self.netD()
+        netD = self.vgg16()
         fakeB = netG(inputA)
+        #outputD = netD(fakeB)
         outputD = netD([inputA, fakeB])
-        netD.tainable = False
+        #netD.tainable = False
         netA = Model(input = [inputA, inputB], output = [fakeB, outputD], name = 'netA')
+        #netA = Model(input = inputB, output = [fakeB, outputD], name = 'netA')
         return netA
 
 class GAN(object):
@@ -122,14 +154,19 @@ class GAN(object):
     lossFuncG = 'mae', lossFuncD = 'binary_crossentropy', lossFuncA1 = 'mae', lossFuncA2 = 'binary_crossentropy', lossRatio = 100, saveFrequency = 5):
         network = networks()
         netG = network.uNet()
-        netD = network.netD()
+        netD = network.vgg16()
+        netA = network.netA()
+        netD.trainble = False
+        netA.compile(optimizer = Adam(lr = 1e-4), loss = [lossFuncA1, lossFuncA2], loss_weights = [lossRatio, 1], metrics = ['accuracy'])
+        netD.trainable = True
         netG.compile(optimizer = Adam(lr = 1e-4), loss = lossFuncG, metrics = ['accuracy'])
         netD.compile(optimizer = Adam(lr = 1e-4), loss = lossFuncD, metrics = ['accuracy'])
-        netA = network.netA()
-        netA.compile(optimizer = Adam(lr = 1e-4), loss = [lossFuncA1, lossFuncA2], loss_weights = [lossRatio, 1], metrics = ['accuracy'])
+        #netD.trainable = False
 
-        extraTrain = dataProc.loadData(inputPath = extraPath, startNum = 0, resize = 1)
-        memTrain = dataProc.loadData(inputPath = memPath, startNum = 0, resize = 1)
+
+
+        extraTrain = dataProc.loadData(inputPath = extraPath, startNum = 0, resize = 1, normalization = 1)
+        memTrain = dataProc.loadData(inputPath = memPath, startNum = 0, resize = 1, normalization = 1)
         extraTrain = extraTrain.reshape((extraTrain.shape[0], self.imgRows, self.imgCols, self.channels))
         memTrain = extraTrain.reshape((memTrain.shape[0], self.imgRows, self.imgCols, self.channels))
 
@@ -141,24 +178,27 @@ class GAN(object):
                 memLocal = memTrain[n:n+batchSize, :]
                 #extraForFakeLocal = extraForFakeMerge[n:n+batchSize, :]
                 #memRealLocal = memRealMerge[n:n+batchSize, :]
-                lossG = netG.train_on_batch(extraLocal, memLocal)
+                #lossG = netG.train_on_batch(extraLocal, memLocal)
                 memFake = netG.predict_on_batch(extraLocal)
                 extraForD = np.concatenate((extraLocal,extraLocal), axis = 0)
                 realFake = np.concatenate((memLocal,memFake), axis = 0)
-                labelD = np.zeros((batchSize*2, 1), dtype = np.uint8)
-                labelD[0:batchSize, :] = 1
-                netD.trainable = True
+                labelD = np.zeros((batchSize*2, 2), dtype = np.float64)
+                labelD[0:batchSize, 0] = 1
+                labelD[batchSize:batchSize*2, 1] = 1
+
                 lossD = netD.train_on_batch([extraForD, realFake], labelD)
-                netD.trainable = False
-                labelA = np.ones((batchSize,1), dtype = np.uint8) #to fool the netD
+                #lossD = netD.train_on_batch(realFake, labelD)
+                labelA = np.ones((batchSize, 2), dtype = np.float64) #to fool the netD
+                labelA[:, 1] = 0
                 lossA = netA.train_on_batch([extraLocal, memLocal], [memLocal, labelA])
-                msg = 'epoch of ' + '%d'%m + ' batch of ' + '%d'%(n/batchSize) + 'lossD=%f'%lossD[0] + 'lossG=%f'%lossG[0]
+                #lossA = netA.train_on_batch(extraLocal, [memLocal, labelA])
+                msg = 'epoch of ' + '%d'%m + ' batch of ' + '%d'%(n/batchSize) + 'lossD=%f'%lossD[0] + 'lossG=%f'%lossA[0]
                 print(msg)
             if m % saveFrequency == (saveFrequency-1):
                 weightsNetGPath = modelPath + 'netG_epoch_%d'%m + '.h5'
                 netG.save_weights(weightsNetGPath, overwrite = True)
-                weightsNetDPath = modelPath + 'netD_epoch_%d'%m + '.h5'
-                netD.save_weights(weightsNetDPath, overwrite = True)
-                weightsNetAPath = modelPath + 'netA_epoch_%d'%m + '.h5'
-                netA.save_weights(weightsNetAPath, overwrite = True)
+                #weightsNetDPath = modelPath + 'netD_epoch_%d'%m + '.h5'
+                #netD.save_weights(weightsNetDPath, overwrite = True)
+                #weightsNetAPath = modelPath + 'netA_epoch_%d'%m + '.h5'
+                #netA.save_weights(weightsNetAPath, overwrite = True)
         print('training completed')
