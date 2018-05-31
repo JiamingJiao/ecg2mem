84576ec0ee3e11bbee2ea421a27dd625431d44e1
JiamingJiao <jiaojm@outlook.com>
2018-05-31 15:26:08 +0900

    cutting connections
    add cutting connections option to model

--- dataProc.py
+++ dataProc.py
@@ -45,41 +45,9 @@ def loadData(inputPath, startNum = 0, resize = 0, rawRows = 200, rawCols = 200, 
         startNum += 1
     if normalization == 1:
         min = np.amin(mergeImg)
-        max = no.amax(mergeImg)
+        max = np.amax(mergeImg)
         mergeImg = (mergeImg-min)/(max-min)
     return mergeImg
-'''
-def generatePseudoECG(srcPath, dstPath, electrodesNum, zDistance = 100, interpolationSize = (200, 200)):
-    src = loadData(inputPath = srcPath, startNum = 0, resize = 0)
-    dst = np.ndarray((src.shape[1], src.shape[2]), dtype = np.float64)
-    diffVKernel = np.zeros((3, 3, 1), dtype = np.float64)
-    diffVKernel[1, :, 0] = 1
-    diffVKernel[:, 1, 0] = 1
-    diffVKernel[1, 1, 0] = -4
-    diffV = np.ndarray(src.shape, dtype = np.float64)
-    distance = np.ndarray((src.shape[1], src.shape[2]), dtype = np.float64)
-    rowDistance = np.ndarray((src.shape[1], src.shape[2]), dtype = np.float64)
-    colDistance = np.ndarray((src.shape[1], src.shape[2]), dtype = np.float64)
-    firstRowIndex = np.linspace(0, src.shape[1], num = src.shape[1], endpoint = False)
-    firstColIndex = np.linspace(0, src.shape[2], num = src.shape[2], endpoint = False)
-    colIndex, rowIndex = np.meshgrid(firstRowIndex, firstColIndex)
-    pseudoECG = np.ndarray((math.floor(math.sqrt(electrodesNum)+0.1), math.floor(math.sqrt(electrodesNum)+0.1)), dtype = np.float64)
-    stride = int(src.shape[1]/(round(math.sqrt(electrodesNum))-1)-1+0.5)
-    zDistanceSquare = zDistance*zDistance
-    for i in range(0, src.shape[0]):
-        diffV = cv.filter2D(src = src[i], ddepth = -1, kernel = diffVKernel, dst = diffV, anchor = (-1, -1), delta = 0, borderType = cv.BORDER_REPLICATE)
-        for siteNum in range(0, electrodesNum):
-            cellRow = math.floor(siteNum/math.sqrt(electrodesNum)+1e-5)
-            cellCol = siteNum%math.floor(math.sqrt(electrodesNum)+1e-5)
-            rowDistance = rowIndex - cellRow * stride
-            colDistance = colIndex - cellCol * stride
-            distance = cv.sqrt(cv.magnitude(rowDistance, rowDistance) + cv.magnitude(colDistance, colDistance) + zDistanceSquare)
-            pseudoECG[cellRow, cellCol] = cv.sumElems(cv.divide(diffV, distance))[0]
-        interpolatedECG = cv.resize(pseudoECG, interpolationSize)
-        dstFileName = dstPath + '%04d'%i
-        np.save(dstFileName, interpolatedECG)
-    print('completed')
-'''
 
 # generate full size pseudo-ECG maps, and downsample them if it is necessary
 def generatePseudoECG(srcPath, dstPath):
@@ -148,3 +116,28 @@ def generateSparsePseudoECG(srcPath, dstPath, samplePoints = (10, 10)):
                 pseudoECG[row, col] = cv.sumElems(cv.divide(diffV, distance))[0]
         interpolated[i, :, :] = cv.resize(pseudoECG, (src.shape[1], src.shape[2]))
     return interpolated
+
+def loadImage(inputPath, startNum = 0, resize = 0, rawRows = 200, rawCols = 200, imgRows = 256, imgCols = 256, normalization = 0):
+    fileName = glob.glob(inputPath + '*.png')
+    if resize == 0:
+        mergeImg = np.ndarray((len(fileName), rawRows, rawCols), dtype = np.float64)
+    else:
+        mergeImg = np.ndarray((len(fileName), imgRows, imgCols), dtype = np.float64)
+        tempImg = np.ndarray((imgRows, imgCols), dtype = np.float64)
+    rawImg = np.ndarray((rawRows, rawCols), dtype = np.float64)
+    for i in range(0, len(fileName)):
+        localName = inputPath + '%04d'%startNum + ".png"
+        rawImg = cv.imread(localName, -1)
+        if resize == 1:
+            #tempImg = cv.resize(rawImg, (imgRows, imgCols))
+            #mergeImg[i] = img_to_array(tempImg)
+            mergeImg[i] = cv.resize(rawImg, (imgRows, imgCols))
+        else:
+            #mergeImg[i] = img_to_array(rawImg)
+            mergeImg[i] = rawImg
+        startNum += 1
+    if normalization == 1:
+        min = np.amin(mergeImg)
+        max = np.amax(mergeImg)
+        mergeImg = (mergeImg-min)/(max-min)
+    return mergeImg
