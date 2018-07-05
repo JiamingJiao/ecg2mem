24c65c7750fbe30c4b9c2f29ba5266d6700c292b
JiamingJiao <jiaojm@outlook.com>
2018-07-06 03:38:31 +0900

    something unfinished about Hist

--- analysis.py
+++ analysis.py
@@ -18,3 +18,13 @@ def calculateMAE(src1, src2):
     absDifference = np.abs(difference)
     mae = np.mean(absDifference)
     return mae
+
+def histEqualizationArray(src, dst):
+    return dst
+
+def calculateHistTransArray(src1, src2, depth = 8):
+    dst = np.zeros((src1.shape[0]), dtype = np.int32)
+    accumulation = 0
+    for i in range(0, 2**depth):
+        dst[i] = Temp
+    return dst
