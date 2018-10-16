7790a3f9eac51c1b57bb43cd0dc2c35e4dc9c070
JiamingJiao <jiaojm@outlook.com>
2018-10-17 01:58:11 +0900

    noooooooooooob

--- iterate.py
+++ iterate.py
@@ -1,29 +1,58 @@
 import os
 import shutil
 import glob
+import json
+import sys
+import numpy as np
 
 import dataProc
 import train
 
-def iterate(simulationDir, trainingDataList, initialElectrodesDir, predictionDataList, **trainArgs):
-    # Store training data temporally in GPUWS, delete it after training.
+def iterate(argsFilename='./iterationArgs.json', **trainArgs):
+    with open(argsFilename) as argsFile:
+        args = json.load(argsFile)
+    simulationDir = args["simulationDir"]
+    trainingDataList = args["train"]["nameList"]
+    tempDir = args["tempDir"]
+
     srcPhieDirList = dataProc.createDirList(simulationDir, trainingDataList, 'phie_')
     srcVmemDirList = dataProc.createDirList(simulationDir, trainingDataList, 'vmem_')
-    phieDirList = dataProc.createDirList('./temp/phie/', trainingDataList)
-    vmemDirList = dataProc.createDirList('./temp/true_vmem/', trainingDataList)
-    for i in range(0, len(trainingDataList)):
+    phieDirList = dataProc.createDirList(tempDir+'phie/', trainingDataList)
+    vmemDirList = dataProc.createDirList(tempDir+'true_vmem/', trainingDataList)
+    for i in range(0, len(args["train"]["nameList"])):
         phieDir = phieDirList[i]
         os.makedirs(phieDir)
         srcPhieFiles = sorted(glob.glob(srcPhieDirList[i]+'*.npy'))
         vmemDir = vmemDirList[i]
         os.makedirs(vmemDir)
         srcVmemFiles = sorted(glob.glob(srcVmemDirList[i]+'*.npy'))
-        for j in range(0, len(files)):
+        for j in range(0, len(srcPhieFiles)):
             shutil.copy(srcPhieFiles[j], phieDir)
             shutil.copy(srcVmemFiles[j], vmemDir)
     print('phie and vmem copied')
-    initialElectrodes = np.load(initialElectrodesDir)
+
+    electrodesDir = args["electrodes"]["dir"]
+    parentElectrodes = np.load(electrodesDir+args["electrodes"]["initial"])
+    i = 0
+    reducedNum = 0
+    mapSize = args["electrodes"]["mapSize"]
+    fullSize = args["electrodes"]["fullSize"]
+    pecgDirList = dataProc.createDirList(tempDir+'pecg/', trainingDataList)
+    while True:
+        electrodesNum = parentElectrodes.shape[0]-reducedNum
+        electrodesPath = args["electrodesDir"]["dir"] + args["experimentName"] + "_%d_%d.npy"%(electrodesNum, i)
+        ecg = dataProc.AccurateSparsePecg(mapSize, reducedNum, fullSize, parentElectrodes, electrodesPath)
+        disablePrint()
+        print('Messages from method: AccurateSparsePecg.resizeAndCalc are muted!')
+        ecg.resizeAndCalc(phieDirList, pecgDirList)
+        enablePrint()
     return 0
+
+def disablePrint():
+    sys.stdout = open(os.devnull, 'w')
+
+def enablePrint():
+    sys.stdout = sys.__stdout__
 
 if __name__ == '__main__':
     iterate()
