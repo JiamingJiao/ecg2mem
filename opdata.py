017a4ca7583cfeaba3c05bf5c2563138f43412a3
JiamingJiao <jiaojm@outlook.com>
2019-01-23 19:37:28 +0900

    add color map for op data

--- opdata.py
+++ opdata.py
@@ -1,6 +1,7 @@
 import numpy as np
 import glob
 import os
+import matplotlib.cm
 
 import dataProc
 
@@ -13,16 +14,23 @@ class OpVmem(dataProc.Vmem):
         self.raw = self.raw.reshape(((len(pathList),) + rawSize + (1,)))
         self.raw = self.raw[:, roi[0][0]:roi[1][0], roi[0][1]:roi[1][1]]
         self.raw = self.raw.astype(np.float64)
+        self.vmem = np.zeros_like(self.raw)
 
-        super(OpVmem, self).__init__(groups=1, length=len(pathList), **kwargs)
-
-    def calcVmem(self):
         # map to [0, 1], different from that in opmap
         rawMax = np.max(self.raw, axis=0)
         rawMin = np.min(self.raw, axis=0)
         rawRange = (rawMax - rawMin) + (rawMax == rawMin)*1
-        self.raw = (rawMax - self.raw) / rawRange
+        self.vmem = (rawMax - self.raw) / rawRange
+
+        self.colorMap = None
+
+        super(OpVmem, self).__init__(groups=1, length=len(pathList), **kwargs)
     
     def reduceNoise(self):
         pass
-        
+    
+    def setColor(self, cmap='inferno'):
+        mapper = matplotlib.cm.ScalarMappable(cmap=cmap)
+        self.colorMap = np.empty((self.length, self.height, self.width, 4), np.float32)
+        for i, frame in enumerate(self.vmem[:, :, :, 0]):
+            self.colorMap[i] = mapper.to_rgba(frame, norm=False)
