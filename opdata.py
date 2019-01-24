f064cc08e58271b03cc2bec2c080f8f3e74ab966
JiamingJiao <jiaojm@outlook.com>
2019-01-24 17:47:31 +0900

    meimaobing

--- opdata.py
+++ opdata.py
@@ -6,25 +6,25 @@ import matplotlib.cm
 import dataProc
 
 class OpVmem(dataProc.Vmem):
-    def __init__(self, srcDir, rawSize, roi, *args, **kwargs):
+    def __init__(self, srcDir, rawSize, roiOrigin, roiSize, *args, **kwargs):
         pathList = sorted(glob.glob(os.path.join(srcDir, '*.raww')))
         self.raw = np.zeros((len(pathList), rawSize[0]*rawSize[1]), np.uint16)
         for i, path in enumerate(pathList):
             self.raw[i, :] = np.fromfile(path, np.uint16)
         self.raw = self.raw.reshape(((len(pathList),) + rawSize + (1,)))
-        self.raw = self.raw[:, roi[0][0]:roi[1][0], roi[0][1]:roi[1][1]]
+        self.raw = self.raw[:, roiOrigin[0]:roiOrigin[0]+roiSize[0], roiOrigin[1]:roiOrigin[1]+roiSize[1]]
         self.raw = self.raw.astype(np.float64)
         self.vmem = np.zeros_like(self.raw)
 
         # map to [0, 1], different from that in opmap
-        rawMax = np.max(self.raw, axis=0)
-        rawMin = np.min(self.raw, axis=0)
-        rawRange = (rawMax - rawMin) + (rawMax == rawMin)*1
-        self.vmem = (rawMax - self.raw) / rawRange
+        self.rawMax = np.amax(self.raw, axis=0)
+        self.rawMin = np.amin(self.raw, axis=0)
+        self.rawRange = (self.rawMax - self.rawMin) + (self.rawMax == self.rawMin)*1
+        self.vmem = (self.rawMax - self.raw) / self.rawRange
 
         self.colorMap = None
 
-        super(OpVmem, self).__init__(groups=1, length=len(pathList), **kwargs)
+        super(OpVmem, self).__init__(groups=1, length=len(pathList), height=roiSize[0], width=roiSize[1], **kwargs)
     
     def reduceNoise(self):
         pass
